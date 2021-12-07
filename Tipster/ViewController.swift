195334dd33275f89a6d097f8ce3fc9fbd68891bf
RAM111111 <r.r.99@windowslive.com>
2021-12-07 16:43:39 +0300

    Initial Commit

---
+++ Tipster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipster
+//
+//  Created by R on 03/05/1443 AH.
+//  Copyright © 1443 R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
